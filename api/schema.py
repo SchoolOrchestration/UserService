af79cd47fb1cb8be3c9508ab0e7f902ae51dd2b2
Spazzy757 <brendankamp757@gmail.com>
2018-02-17 12:17:24 +0200

    Fixed bug

--- api/schema.py
+++ api/schema.py
@@ -1,3 +1,7 @@
+"""
+These schemas are for the Function Based Views Until a time that Django
+Rest Swagger supports them. Otherwise use the Auto generated Schemas
+"""
 from rest_framework.schemas import ManualSchema
 import coreapi
 import uuid
@@ -8,10 +12,13 @@ user_login_schema = ManualSchema(
             "body",
             required=True,
             location="body",
+            type="string",
             schema={
                 'username': 'test_user',
                 'password': str(uuid.uuid4())
-            }
+            },
+            description=("A JSON object containing a username and password "
+                         "pair")
         ),
     ]
 )
