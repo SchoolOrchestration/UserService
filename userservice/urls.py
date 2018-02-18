e2a33038407d44328c671198447a09b659d29aa9
Spazzy757 <brendankamp757@gmail.com>
2018-02-18 11:30:33 +0200

    Changed to swagger 2.1.1

--- userservice/urls.py
+++ userservice/urls.py
@@ -13,20 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include
+from rest_framework_swagger.views import get_swagger_view
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 from api import api, schema
 
+schema_view = get_swagger_view(title='Demo Swagger API')
 
 urlpatterns = [
+    url(r'^docs/', schema_view),
     path('admin/', admin.site.urls),
-    path(
-        'docs/',
-        schema.get_swagger_view(schema_location='api.schema.'
-                                                'user_login_schema'),
-        name='docs'
-    ),
     path('login', api.get_user_info, name='login'),
     path('', include('api.urls'), name='api')
 ]
