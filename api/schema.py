f277bf20ab8cd5830db11039d549632d6d20437b
Spazzy757 <brendankamp757@gmail.com>
2018-02-18 14:57:32 +0200

    Added example field

--- api/schema.py
+++ api/schema.py
@@ -4,11 +4,12 @@ import coreschema
 
 
 user_login_schema = schemas.AutoSchema(manual_fields=[
-        coreapi.Field(
-            "body",
-            required=True,
-            location="body",
-            schema=coreschema.Object(),
-            description="Returns a user object after authenticating"
-        ),
-    ])
+    coreapi.Field(
+        "body",
+        required=True,
+        location="body",
+        schema=coreschema.Object(),
+        description="Returns a user object after authenticating",
+        example="",
+    ),
+])
